2edfaabf1b1eb247d96c20510e0b5d9f310dbdc0
Shimamotogit <134857097+Shimamotogit@users.noreply.github.com>
2026-07-27 03:46:51 +0900

    feat: wire launch flow and media extensions

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-const { initializeQuestionSource } = await import("./question-source.js");
+import {
+  configureQuestionSource,
+  initializeQuestionSource
+} from "./question-source.js";
+import {
+  initializeLaunchFlow,
+  installReturnToTitleHandler
+} from "./launch-flow.js";
+
 await initializeQuestionSource();
+const launchOptions = await initializeLaunchFlow();
+configureQuestionSource({ difficulty: launchOptions.difficulty });
+
 await import("./detector-compat.js");
 await import("./app.js");
+
+const { initializeQuizMedia } = await import("./quiz-media.js");
+await initializeQuizMedia();
+installReturnToTitleHandler();
