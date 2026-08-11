922a2ae5c2acdaf8219f0685043055079cdf2a34
Shimamotogit <134857097+Shimamotogit@users.noreply.github.com>
2026-08-12 03:23:11 +0900

    VOICEVOXクレジットを初期表示

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ import {
 } from "./question-speech.js";
 import { initializeSystemAudioFlow } from "./system-audio-flow.js";
 import { installSystemAudioAlias } from "./system-audio-alias.js";
+import { installVoicevoxCredit } from "./voicevox-credit.js";
 
 async function loadChoiceDisplayFixStyles() {
   const existing = document.querySelector('link[data-choice-display-fixes="true"]');
@@ -39,6 +40,7 @@ async function loadChoiceDisplayFixStyles() {
 installAnswerTimingGuard();
 installQuestionSpeechClock();
 installSystemAudioAlias();
+installVoicevoxCredit();
 await loadChoiceDisplayFixStyles();
 await initializeQuestionSource();
 const launchOptions = await initializeLaunchFlow();
